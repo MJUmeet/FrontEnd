ef8c51f8dd094d1e3068b608261d2c52ba57ec5f
snowari <pes1149@naver.com>
2022-12-11 00:40:04 +0900

    feat:service

---
+++ src/service/index.js
@@ -0,0 +1,87 @@
+import Axios from '../axios';
+
+export default {
+  createUser(id, passwd, name) {
+    return Axios({
+      url: '/api/signup',
+      method: 'post',
+      data: {
+        id,
+        passwd,
+        name,
+      },
+    });
+  },
+
+  login(id, passwd) {
+    return Axios({
+      url: 'api/login',
+      method: 'post',
+      data: {
+        id,
+        passwd,
+      },
+    });
+  },
+
+  createRoom(id, roomName, meetingTime) {
+    return Axios({
+      url: '/api/room',
+      method: 'post',
+      data: {
+        id,
+        roomName,
+        meetingTime,
+      },
+    });
+  },
+
+  lookupRoom(id) {
+    return Axios({
+      url: 'api/rooms',
+      method: 'post',
+      data: {
+        id,
+      },
+    });
+  },
+
+  enrollMember(roomId, userId) {
+    return Axios({
+      url: 'api/room/user',
+      method: 'post',
+      data: {
+        roomId,
+        userId,
+      },
+    });
+  },
+
+  saveTimetable(
+    userId,
+    roomId,
+    mondayTimetable,
+    tuesdayTimetable,
+    wednesdayTimetable,
+    thursdayTimetable,
+    fridayTimetable,
+    saturdayTimetable,
+    sundayTimetable,
+  ) {
+    return Axios({
+      url: 'api/timetable',
+      method: 'post',
+      data: {
+        userId,
+        roomId,
+        mondayTimetable,
+        tuesdayTimetable,
+        wednesdayTimetable,
+        thursdayTimetable,
+        fridayTimetable,
+        saturdayTimetable,
+        sundayTimetable,
+      },
+    });
+  },
+};
